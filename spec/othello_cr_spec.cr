8bee0debf117e4de77cac1c8753a0db70259ef79
aurumcodex <aurumcodex@protonmail.com>
2020-11-04 15:44:23 -0500

    initial commit

---
+++ spec/othello_cr_spec.cr
@@ -0,0 +1,10 @@
+require "./spec_helper"
+
+describe OthelloCr do
+  # TODO: Write tests
+
+  it "works" do
+    bo = true
+    bo.should eq(true)
+  end
+end
---
+++ src/othello.cr
@@ -0,0 +1,6 @@
+# TODO: Write documentation for `OthelloCr`
+module Othello
+  VERSION = "0.1.0"
+
+  puts "hello"
+end
